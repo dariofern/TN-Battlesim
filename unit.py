24392170b04766527787241d2481a16e8c1db7cb
JosiahWI <josiah_vanderzee@mediacombb.net>
2021-07-31 11:54:51 -0500

    load from stream instead of filename

--- unit.py
+++ unit.py
@@ -5,7 +5,7 @@ import csv
 
 Unit = namedtuple("Unit", ("unit_class", "name", "defense", "attack"))
 
-def load_unit_data(unit_data_file):
+def load_unit_data(handle):
     """
     Loads data from CSV into a list of unit types.
 
@@ -13,11 +13,10 @@ def load_unit_data(unit_data_file):
     Unit Class, Unit Name, Defense, and Attack (order does not matter).
     """
     units = []
-    with open(unit_data_file) as f:
-        for row in csv.DictReader(f):
-            unit_class = row["Unit Class"]
-            units.append(Unit(row["Unit Class"], row["Unit Name"],
-                         float(row["Defense"]), float(row["Attack"])))
+    for row in csv.DictReader(handle):
+        unit_class = row["Unit Class"]
+        units.append(Unit(row["Unit Class"], row["Unit Name"],
+                        float(row["Defense"]), float(row["Attack"])))
 
     return units
 
@@ -26,4 +25,5 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: ./unit.py <path_to_unit_data>")
     else:
-        print(load_unit_data(sys.argv[1]))
+        with open(sys.argv[1]) as f:
+            print(load_unit_data(f))
